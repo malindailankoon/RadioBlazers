12fd26a6c308318172eca83d35aa1fbc9002f5d1
Malinda Ilankoon <malindailankoon2003@gmail.com>
2025-10-07 16:20:12 +0530

    added full modulation to the knife

--- Simulations/Playground/Malinda/knife/threading1.py
+++ Simulations/Playground/Malinda/knife/threading1.py
@@ -10,12 +10,14 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
+from PyQt5 import QtCore
 from gnuradio import blocks
 import pmt
 from gnuradio import blocks, gr
+from gnuradio import channels
+from gnuradio.filter import firdes
 from gnuradio import digital
 from gnuradio import gr
-from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
@@ -72,16 +74,31 @@ class threading1(gr.top_block, Qt.QWidget):
         self.nfilts = nfilts = 32
         self.access_key = access_key = '11100001010110101110100010010011'
         self.variable_adaptive_algorithm_0 = variable_adaptive_algorithm_0 = digital.adaptive_algorithm_cma( qpsk, .0001, 4).base()
+        self.usrp_rate = usrp_rate = 768000
+        self.time_offset = time_offset = 1.000
         self.thresh = thresh = 1
-        self.taps = taps = [1.0, 0.25-0.25j, 0.50 + 0.10j, -0.3 + 0.2j]
+        self.taps = taps = [1.0 + 0.0j, ]
         self.samp_rate = samp_rate = 32000
         self.rrc_taps = rrc_taps = firdes.root_raised_cosine(nfilts, nfilts, 1.0/float(sps), 0.35, 11*sps*nfilts)
+        self.phase_bw = phase_bw = 0.0628
+        self.noise_volt = noise_volt = 0
         self.hdr_format = hdr_format = digital.header_format_default(access_key, 0)
+        self.freq_offset = freq_offset = 0
+        self.excess_bw = excess_bw = 0.35
 
         ##################################################
         # Blocks
         ##################################################
 
+        self._time_offset_range = qtgui.Range(0.999, 1.001, 0.0001, 1.000, 200)
+        self._time_offset_win = qtgui.RangeWidget(self._time_offset_range, self.set_time_offset, "Timing Offset", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._time_offset_win)
+        self._noise_volt_range = qtgui.Range(0, 1, 0.01, 0, 200)
+        self._noise_volt_win = qtgui.RangeWidget(self._noise_volt_range, self.set_noise_volt, "Noise Voltage", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._noise_volt_win)
+        self._freq_offset_range = qtgui.Range(-0.1, 0.1, 0.001, 0, 200)
+        self._freq_offset_win = qtgui.RangeWidget(self._freq_offset_range, self.set_freq_offset, "Frequency Offset", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._freq_offset_win)
         self.pdu_tagged_stream_to_pdu_0_0_0 = pdu.tagged_stream_to_pdu(gr.types.byte_t, 'packet_len')
         self.pdu_tagged_stream_to_pdu_0_0 = pdu.tagged_stream_to_pdu(gr.types.byte_t, 'packet_len')
         self.pdu_pdu_to_tagged_stream_0_1 = pdu.pdu_to_tagged_stream(gr.types.byte_t, 'packet_len')
@@ -92,18 +109,62 @@ class threading1(gr.top_block, Qt.QWidget):
         self.epy_block_0 = epy_block_0.blk(node_id=1, aloha_prob=0.5, timeout=1, max_retries=3)
         self.digital_protocol_formatter_async_0_0 = digital.protocol_formatter_async(hdr_format)
         self.digital_protocol_formatter_async_0 = digital.protocol_formatter_async(hdr_format)
+        self.digital_pfb_clock_sync_xxx_0_0 = digital.pfb_clock_sync_ccf(sps, phase_bw, rrc_taps, nfilts, (nfilts/2), 1.5, 2)
+        self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, phase_bw, rrc_taps, nfilts, (nfilts/2), 1.5, 2)
+        self.digital_map_bb_0_1_0 = digital.map_bb([0,1,2,3])
+        self.digital_map_bb_0_1 = digital.map_bb([0,1,2,3])
+        self.digital_linear_equalizer_0_0 = digital.linear_equalizer(15, 2, variable_adaptive_algorithm_0, True, [ ], 'corr_est')
+        self.digital_linear_equalizer_0 = digital.linear_equalizer(15, 2, variable_adaptive_algorithm_0, True, [ ], 'corr_est')
+        self.digital_diff_decoder_bb_0_1_0 = digital.diff_decoder_bb(4, digital.DIFF_DIFFERENTIAL)
+        self.digital_diff_decoder_bb_0_1 = digital.diff_decoder_bb(4, digital.DIFF_DIFFERENTIAL)
+        self.digital_costas_loop_cc_0_1_0 = digital.costas_loop_cc(phase_bw, 4, False)
+        self.digital_costas_loop_cc_0_1 = digital.costas_loop_cc(phase_bw, 4, False)
         self.digital_correlate_access_code_xx_ts_0_0 = digital.correlate_access_code_bb_ts("11100001010110101110100010010011",
           thresh, 'packet_len')
         self.digital_correlate_access_code_xx_ts_0 = digital.correlate_access_code_bb_ts("11100001010110101110100010010011",
           thresh, 'packet_len')
-        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_char*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.digital_constellation_modulator_0_0_0 = digital.generic_mod(
+            constellation=qpsk,
+            differential=True,
+            samples_per_symbol=sps,
+            pre_diff_code=True,
+            excess_bw=excess_bw,
+            verbose=False,
+            log=False,
+            truncate=False)
+        self.digital_constellation_modulator_0_0 = digital.generic_mod(
+            constellation=qpsk,
+            differential=True,
+            samples_per_symbol=sps,
+            pre_diff_code=True,
+            excess_bw=excess_bw,
+            verbose=False,
+            log=False,
+            truncate=False)
+        self.digital_constellation_decoder_cb_0_1_0 = digital.constellation_decoder_cb(qpsk)
+        self.digital_constellation_decoder_cb_0_1 = digital.constellation_decoder_cb(qpsk)
+        self.channels_channel_model_0_0 = channels.channel_model(
+            noise_voltage=noise_volt,
+            frequency_offset=freq_offset,
+            epsilon=time_offset,
+            taps=taps,
+            noise_seed=0,
+            block_tags=True)
+        self.channels_channel_model_0 = channels.channel_model(
+            noise_voltage=noise_volt,
+            frequency_offset=freq_offset,
+            epsilon=time_offset,
+            taps=taps,
+            noise_seed=0,
+            block_tags=True)
+        self.blocks_unpack_k_bits_bb_0_0 = blocks.unpack_k_bits_bb(2)
+        self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(2)
+        self.blocks_throttle2_0_0_0_0 = blocks.throttle( gr.sizeof_gr_complex*1, usrp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * usrp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_throttle2_0_0_0 = blocks.throttle( gr.sizeof_gr_complex*1, usrp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * usrp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_tagged_stream_mux_0_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
         self.blocks_tagged_stream_mux_0 = blocks.tagged_stream_mux(gr.sizeof_char*1, 'packet_len', 0)
-        self.blocks_repack_bits_bb_1_0_1 = blocks.repack_bits_bb(8, 1, "packet_len", False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_1_0_0_0 = blocks.repack_bits_bb(1, 8, "packet_len", False, gr.GR_MSB_FIRST)
         self.blocks_repack_bits_bb_1_0_0 = blocks.repack_bits_bb(1, 8, "packet_len", False, gr.GR_MSB_FIRST)
-        self.blocks_repack_bits_bb_1_0 = blocks.repack_bits_bb(8, 1, "packet_len", False, gr.GR_MSB_FIRST)
         self.blocks_message_strobe_0_0 = blocks.message_strobe(pmt.intern("1:Hello from B"), 1000)
         self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("2:Hello from A"), 1000)
         self.blocks_message_debug_0_0 = blocks.message_debug(True, gr.log_levels.info)
@@ -125,16 +186,32 @@ class threading1(gr.top_block, Qt.QWidget):
         self.msg_connect((self.epy_block_0_0, 'pdu_out'), (self.digital_protocol_formatter_async_0_0, 'in'))
         self.msg_connect((self.pdu_tagged_stream_to_pdu_0_0, 'pdus'), (self.epy_block_0_0, 'pdu_in'))
         self.msg_connect((self.pdu_tagged_stream_to_pdu_0_0_0, 'pdus'), (self.epy_block_0, 'pdu_in'))
-        self.connect((self.blocks_repack_bits_bb_1_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_repack_bits_bb_1_0_0, 0), (self.pdu_tagged_stream_to_pdu_0_0, 0))
         self.connect((self.blocks_repack_bits_bb_1_0_0_0, 0), (self.pdu_tagged_stream_to_pdu_0_0_0, 0))
-        self.connect((self.blocks_repack_bits_bb_1_0_1, 0), (self.blocks_throttle2_0_0, 0))
-        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.blocks_repack_bits_bb_1_0, 0))
-        self.connect((self.blocks_tagged_stream_mux_0_0, 0), (self.blocks_repack_bits_bb_1_0_1, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
-        self.connect((self.blocks_throttle2_0_0, 0), (self.digital_correlate_access_code_xx_ts_0_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0, 0), (self.digital_constellation_modulator_0_0, 0))
+        self.connect((self.blocks_tagged_stream_mux_0_0, 0), (self.digital_constellation_modulator_0_0_0, 0))
+        self.connect((self.blocks_throttle2_0_0_0, 0), (self.channels_channel_model_0, 0))
+        self.connect((self.blocks_throttle2_0_0_0_0, 0), (self.channels_channel_model_0_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.digital_correlate_access_code_xx_ts_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0_0, 0), (self.digital_correlate_access_code_xx_ts_0_0, 0))
+        self.connect((self.channels_channel_model_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
+        self.connect((self.channels_channel_model_0_0, 0), (self.digital_pfb_clock_sync_xxx_0_0, 0))
+        self.connect((self.digital_constellation_decoder_cb_0_1, 0), (self.digital_diff_decoder_bb_0_1, 0))
+        self.connect((self.digital_constellation_decoder_cb_0_1_0, 0), (self.digital_diff_decoder_bb_0_1_0, 0))
+        self.connect((self.digital_constellation_modulator_0_0, 0), (self.blocks_throttle2_0_0_0, 0))
+        self.connect((self.digital_constellation_modulator_0_0_0, 0), (self.blocks_throttle2_0_0_0_0, 0))
         self.connect((self.digital_correlate_access_code_xx_ts_0, 0), (self.blocks_repack_bits_bb_1_0_0, 0))
         self.connect((self.digital_correlate_access_code_xx_ts_0_0, 0), (self.blocks_repack_bits_bb_1_0_0_0, 0))
+        self.connect((self.digital_costas_loop_cc_0_1, 0), (self.digital_constellation_decoder_cb_0_1, 0))
+        self.connect((self.digital_costas_loop_cc_0_1_0, 0), (self.digital_constellation_decoder_cb_0_1_0, 0))
+        self.connect((self.digital_diff_decoder_bb_0_1, 0), (self.digital_map_bb_0_1, 0))
+        self.connect((self.digital_diff_decoder_bb_0_1_0, 0), (self.digital_map_bb_0_1_0, 0))
+        self.connect((self.digital_linear_equalizer_0, 0), (self.digital_costas_loop_cc_0_1, 0))
+        self.connect((self.digital_linear_equalizer_0_0, 0), (self.digital_costas_loop_cc_0_1_0, 0))
+        self.connect((self.digital_map_bb_0_1, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.digital_map_bb_0_1_0, 0), (self.blocks_unpack_k_bits_bb_0_0, 0))
+        self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_linear_equalizer_0, 0))
+        self.connect((self.digital_pfb_clock_sync_xxx_0_0, 0), (self.digital_linear_equalizer_0_0, 0))
         self.connect((self.pdu_pdu_to_tagged_stream_0, 0), (self.blocks_tagged_stream_mux_0, 0))
         self.connect((self.pdu_pdu_to_tagged_stream_0_0, 0), (self.blocks_tagged_stream_mux_0, 1))
         self.connect((self.pdu_pdu_to_tagged_stream_0_0_0, 0), (self.blocks_tagged_stream_mux_0_0, 0))
@@ -161,6 +238,8 @@ class threading1(gr.top_block, Qt.QWidget):
 
     def set_qpsk(self, qpsk):
         self.qpsk = qpsk
+        self.digital_constellation_decoder_cb_0_1.set_constellation(self.qpsk)
+        self.digital_constellation_decoder_cb_0_1_0.set_constellation(self.qpsk)
 
     def get_nfilts(self):
         return self.nfilts
@@ -182,6 +261,22 @@ class threading1(gr.top_block, Qt.QWidget):
     def set_variable_adaptive_algorithm_0(self, variable_adaptive_algorithm_0):
         self.variable_adaptive_algorithm_0 = variable_adaptive_algorithm_0
 
+    def get_usrp_rate(self):
+        return self.usrp_rate
+
+    def set_usrp_rate(self, usrp_rate):
+        self.usrp_rate = usrp_rate
+        self.blocks_throttle2_0_0_0.set_sample_rate(self.usrp_rate)
+        self.blocks_throttle2_0_0_0_0.set_sample_rate(self.usrp_rate)
+
+    def get_time_offset(self):
+        return self.time_offset
+
+    def set_time_offset(self, time_offset):
+        self.time_offset = time_offset
+        self.channels_channel_model_0.set_timing_offset(self.time_offset)
+        self.channels_channel_model_0_0.set_timing_offset(self.time_offset)
+
     def get_thresh(self):
         return self.thresh
 
@@ -193,26 +288,60 @@ class threading1(gr.top_block, Qt.QWidget):
 
     def set_taps(self, taps):
         self.taps = taps
+        self.channels_channel_model_0.set_taps(self.taps)
+        self.channels_channel_model_0_0.set_taps(self.taps)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
 
     def get_rrc_taps(self):
         return self.rrc_taps
 
     def set_rrc_taps(self, rrc_taps):
         self.rrc_taps = rrc_taps
+        self.digital_pfb_clock_sync_xxx_0.update_taps(self.rrc_taps)
+        self.digital_pfb_clock_sync_xxx_0_0.update_taps(self.rrc_taps)
+
+    def get_phase_bw(self):
+        return self.phase_bw
+
+    def set_phase_bw(self, phase_bw):
+        self.phase_bw = phase_bw
+        self.digital_costas_loop_cc_0_1.set_loop_bandwidth(self.phase_bw)
+        self.digital_costas_loop_cc_0_1_0.set_loop_bandwidth(self.phase_bw)
+        self.digital_pfb_clock_sync_xxx_0.set_loop_bandwidth(self.phase_bw)
+        self.digital_pfb_clock_sync_xxx_0_0.set_loop_bandwidth(self.phase_bw)
+
+    def get_noise_volt(self):
+        return self.noise_volt
+
+    def set_noise_volt(self, noise_volt):
+        self.noise_volt = noise_volt
+        self.channels_channel_model_0.set_noise_voltage(self.noise_volt)
+        self.channels_channel_model_0_0.set_noise_voltage(self.noise_volt)
 
     def get_hdr_format(self):
         return self.hdr_format
 
     def set_hdr_format(self, hdr_format):
         self.hdr_format = hdr_format
+
+    def get_freq_offset(self):
+        return self.freq_offset
+
+    def set_freq_offset(self, freq_offset):
+        self.freq_offset = freq_offset
+        self.channels_channel_model_0.set_frequency_offset(self.freq_offset)
+        self.channels_channel_model_0_0.set_frequency_offset(self.freq_offset)
+
+    def get_excess_bw(self):
+        return self.excess_bw
+
+    def set_excess_bw(self, excess_bw):
+        self.excess_bw = excess_bw
 
 
 
